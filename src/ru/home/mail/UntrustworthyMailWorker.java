7a1cfd263f413b075ffe1b56755f08eef68e0b33
amsterdam32 <v.postman2014@yandex.com>
2016-04-14 07:54:17 +0300

    Some first commit

---
+++ src/ru/home/mail/UntrustworthyMailWorker.java
@@ -0,0 +1,24 @@
+package ru.home.mail;
+
+/**
+ * Created by Wladimir on 13.04.2016.
+ */
+public class UntrustworthyMailWorker implements MailService {
+    Sendable mail;
+
+    public UntrustworthyMailWorker(MailService[] arr, Sendable mail) {
+        for(int i=0;i<arr.length;i++){
+            mail = arr[i].processMail(mail);
+        }
+        this.mail = mail;
+    }
+
+    public RealMailService getRealMailService (){
+        return this.processMail(mail);
+    }
+
+    @Override
+    public Sendable processMail(Sendable mail) {
+        return mail;
+    }
+}
